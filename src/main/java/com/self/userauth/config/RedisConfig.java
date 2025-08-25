512aef9d23f51dffed4c1117ce0b03e14ebafd5e
SapnaS01 <87744431+SapnaS01@users.noreply.github.com>
2025-08-25 16:28:16 +0530

    fix: replace in memory cache to redis in authservice

--- src/main/java/com/self/userauth/config/RedisConfig.java
+++ src/main/java/com/self/userauth/config/RedisConfig.java
@@ -1,18 +1,37 @@
 package com.self.userauth.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@RequiredArgsConstructor
 public class RedisConfig {
-	
-	@Bean
-	RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-        return template;
-    }
 
+	private final ObjectMapper objectMapper;
+
+
+	@Bean
+	LettuceConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory();
+	}
+
+	@Bean
+	RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory factory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(factory);
+
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+
+		template.afterPropertiesSet();
+		return template;
+	}
 }
